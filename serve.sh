5f4dcd0f291bed911cdd3eb3da3a4dc1c524889d
objz <me@objz.dev>
2025-05-28 10:24:44 +0200

    fixed build.sh

--- serve.sh
+++ serve.sh
@@ -3,9 +3,9 @@ wasm-pack build --target web --out-dir pkg
 rm -rf dist 
 mkdir -p dist/js
 mkdir -p dist/style
-cp static/index.html dist/
-cp style/styles.css dist/style/
-cp js/index.js dist/js/
+cp -r static/* dist/
+cp -r style/* dist/style/
+cp -r js/* dist/js/
 cp -r pkg dist/
 npx serve dist
 
